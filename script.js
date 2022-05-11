13cde52ef0163a90930f543041f91402f2b3974a
Yaroslav <ippolitov_yaroslav@mail.ru>
2022-05-11 23:58:50 +0300

    added dialog and logging

--- script.js
+++ script.js
@@ -1,74 +1,142 @@
 const args = process.argv;
 const mode = process.env.mode;
 const platform = process.platform;
-const a = args[2];
-const b = args[3];
-const c = args[4];
+let a, b, c;
 let isWindows = platform.search("win");
 let nodePath = "";
 let scriptPath = "";
+const path = require("path");
+const fs = require("fs");
+const resultFilePath = path.join(path.dirname(process.argv[1]), "log.txt");
 
-if (isWindows === -1) {
-  isWindows = false;
-} else {
-  isWindows = true;
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function checkOS() {
+  if (isWindows === -1) {
+    isWindows = false;
+  } else {
+    isWindows = true;
+  }
 }
 
-if (isWindows) {
-  nodePath = args[0].split("\\").pop();
-  scriptPath = args[1].split("\\").pop();
-  nodePath = nodePath.replace(".exe", "");
-} else {
-  nodePath = args[0].split("/").pop();
-  scriptPath = args[1].split("/").pop();
+function fullfillPaths() {
+  if (isWindows) {
+    nodePath = args[0].split("\\").pop();
+    scriptPath = args[1].split("\\").pop();
+    nodePath = nodePath.replace(".exe", "");
+  } else {
+    nodePath = args[0].split("/").pop();
+    scriptPath = args[1].split("/").pop();
+  }
 }
 
-if (a === undefined || b === undefined || c === undefined) {
+function printGreeting() {
+  // console.log(
+  //   "Для решения квадртаного уравнения aх^2+bx+c = 0 задайте коэффициенты a, b, c, как аргументы скрипта."
+  // );
   console.log(
-    "Для решения квадртаного уравнения aх^2+bx+c = 0 задайте коэффициенты a, b, c, как аргументы скрипта."
-  );
-  console.log(
-    `Наример, для решения уравнения 4x^2-16x+10=0 введите команду ${nodePath} ${scriptPath} 4 -16 10`
+    `Для решения квадртаного уравнения  введите команду ${nodePath} ${scriptPath}`
   );
   console.log(
     `Для детального отображения решения используйте параметр MODE=details`
   );
-  process.exit(1);
 }
 
-if (a == 0) {
-  if (b == 0) {
-    console.log(`Заданы некорректные параметры.`);
-    process.exit(3);
+function calculateEquation() {
+  if (a === undefined || b === undefined || c === undefined) {
+    process.exit(1);
   }
-  const x = -c / b;
+  if (a == 0) {
+    if (b == 0) {
+      result = `Заданы некорректные параметры.`;
+      console.log(result);
+      writeToLog(result);
+      process.exit(3);
+    }
+    const x = -c / b;
+    if (mode == "details") {
+      console.log(`x = ${-c} /${b} = ${x}`);
+    }
+    result = `Ответ: x = ${x}.`;
+    console.log(result);
+    writeToLog(result);
+
+    process.exit(0);
+  }
+
+  const D = Math.pow(b, 2) - 4 * a * c;
   if (mode == "details") {
-    console.log(`x = ${-c} /${b} = ${x}`);
+    console.log(`D = ${b}^2 - 4*${a}*${c} = ${D}`);
   }
-  console.log(`Ответ: x = ${x}.`);
+  if (D < 0) {
+    result = "Корней нет.";
+    console.log(result);
+    writeToLog(result);
+  } else if (D === 0) {
+    const x = -b / (2 * a);
+    if (mode == "details") {
+      console.log(`x = -${b}/(2 * ${a}) = ${x}`);
+    }
+    result = `Ответ: x = ${x}.`;
+    console.log(result);
+    writeToLog(result);
+  } else if (D > 0) {
+    const x1 = (-b + Math.sqrt(D)) / (2 * a);
+    const x2 = (-b - Math.sqrt(D)) / (2 * a);
+    if (mode == "details") {
+      console.log(`x1 = -${b} + sqrt(${D})/(2 * ${a}) = ${x1}`);
+      console.log(`x2 = -${b} - sqrt(${D})/(2 * ${a}) = ${x2}`);
+    }
+    result = `Ответ: x1 = ${x1}, x2 = ${x2}.`;
+    console.log(result);
+    writeToLog(result);
+  }
   process.exit(0);
 }
-
-const D = Math.pow(b, 2) - 4 * a * c;
-if (mode == "details") {
-  console.log(`D = ${b}^2 - 4*${a}*${c} = ${D}`);
+function writeToLog(logString) {
+  logString = logString + "\n";
+  fs.writeFileSync(resultFilePath, logString, { flag: "a" });
 }
-if (D < 0) {
-  console.log("Корней нет.");
-} else if (D === 0) {
-  const x = -b / (2 * a);
-  if (mode == "details") {
-    console.log(`x = -${b}/(2 * ${a}) = ${x}`);
-  }
-  console.log(`Ответ: x = ${x}.`);
-} else if (D > 0) {
-  const x1 = (-b + Math.sqrt(D)) / (2 * a);
-  const x2 = (-b - Math.sqrt(D)) / (2 * a);
-  if (mode == "details") {
-    console.log(`x1 = -${b} + sqrt(${D})/(2 * ${a}) = ${x1}`);
-    console.log(`x2 = -${b} - sqrt(${D})/(2 * ${a}) = ${x2}`);
-  }
-  console.log(`Ответ: x1 = ${x1}, x2 = ${x2}.`);
+function fullfullA(answer) {
+  a = answer;
+  const logA = `a = ${a}`;
+  writeToLog(logA);
+  rl.question("Enter b:", fullfullB);
 }
 
-process.exit(0);
+function fullfullB(answer) {
+  b = answer;
+  const logB = `b = ${b}`;
+  writeToLog(logB);
+  rl.question("Enter c:", fullfullC);
+}
+
+function fullfullC(answer) {
+  c = answer;
+  const logC = `c = ${c}`;
+  writeToLog(logC);
+
+  calculateEquation();
+  rl.on("close", () => {
+    console.log(`Done. Result file: ${resultFilePath}`);
+  });
+}
+
+function showUserDialog() {
+  fs.unlinkSync(resultFilePath);
+  rl.question("Enter a:", fullfullA);
+}
+
+function main() {
+  checkOS();
+  fullfillPaths();
+  printGreeting();
+  showUserDialog();
+}
+
+main();
